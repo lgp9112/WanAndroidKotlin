0b69d6d79abf28d532fbf6adcd757c2fa9e786b3
lgp <luoguopeng@xa.com>
2020-12-24 00:16:48 +0800

    DrawerLayout沉浸式状态栏 1.content部分是FrameLayout/RelativeLayout/LinearLayout 第一步：xml中DrawerLayout设置android:fitsSystemWindows="false" 第二步：xml中FrameLayout/RelativeLayout/LinearLayout设置android:fitsSystemWindows="true" 第三步：代码中设置BarUtils.transparentStatusBar(activity)
    2.content部分是CoordinatorLayout
    第一步：xml中DrawerLayout设置android:fitsSystemWindows="true"
    第二步：代码中设置BarUtils.transparentStatusBar(activity)
    第三步：代码中设置DrawerLayout和DrawerLayout子View的fitsSystemWindows=false
        drawer.fitsSystemWindows = false
        var i = 0
        val count = drawer.childCount
        while (i < count) {
            drawer.getChildAt(i).fitsSystemWindows = false
            i++
        }

---
+++ app/src/main/java/com/lgp/wanandroid/kotlin/util/Ext.kt
@@ -0,0 +1,31 @@
+package com.lgp.wanandroid.kotlin
+
+import android.app.Activity
+import android.os.Build
+import androidx.drawerlayout.widget.DrawerLayout
+import com.blankj.utilcode.util.BarUtils
+
+
+/**
+ * 创建者     罗国鹏
+ * 创建时间   2020/12/23 23:32
+ * 描述
+ * 更新者     $
+ * 更新时间   $
+ * 更新描述
+ */
+fun transparentStatusBar4Drawer(
+    activity: Activity,
+    drawer: DrawerLayout,
+) {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) return
+
+    BarUtils.transparentStatusBar(activity)
+    drawer.fitsSystemWindows = false
+    var i = 0
+    val count = drawer.childCount
+    while (i < count) {
+        drawer.getChildAt(i).fitsSystemWindows = false
+        i++
+    }
+}
